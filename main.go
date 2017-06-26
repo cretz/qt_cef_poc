8fc878d382eac89dee9c32da8ad59b831b290cde
Chad Retz <chad.retz@gmail.com>
2017-06-26 18:06:30 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	if err := start(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(2)
+	}
+}
+
+var isWindows = runtime.GOOS == "windows"
+var debugEnabled = false
+
+func start() error {
+	// Only supporting Windows 64 bit for now
+	if !isWindows || runtime.GOARCH != "amd64" {
+		return fmt.Errorf("Unsupported OS '%v' or arch '%v'", runtime.GOOS, runtime.GOARCH)
+	}
+	if len(os.Args) < 2 {
+		return fmt.Errorf("No command provided. Use either 'clean', 'build', or 'run'")
+	}
+
+	switch os.Args[1] {
+	case "run":
+		return run()
+	case "clean":
+		return clean()
+	case "build":
+		return build()
+	default:
+		return fmt.Errorf("Unrecognized command '%v'", os.Args[1])
+	}
+}
+
+func run() error {
+	if err := build(); err != nil {
+		return err
+	}
+	return execCmd(filepath.Join(os.Args[2], "qt_cef_poc.exe"))
+}
+
+func clean() error {
+	err := os.RemoveAll("debug")
+	if err == nil {
+		err = os.RemoveAll("release")
+	}
+	return err
+}
+
+func build() error {
+	if len(os.Args) < 3 || (os.Args[2] != "release" && os.Args[2] != "debug") {
+		return fmt.Errorf("Unknown target")
+	}
+	target := os.Args[2]
+	// Get qmake path
+	qmakeExeName := "qmake"
+	if isWindows {
+		qmakeExeName += ".exe"
+	}
+	qmakePath, err := exec.LookPath(qmakeExeName)
+	if err != nil {
+		return err
+	}
+
+	// Make the dir for the target
+	if err := os.MkdirAll(target, os.ModeDir); err != nil {
+		return err
+	}
+
+	// Run qmake TODO: put behind flag
+	if err := execCmd(qmakePath, "qt_cef_poc.pro"); err != nil {
+		return fmt.Errorf("QMake failed: %v", err)
+	}
+
+	// Run nmake
+	nmakePath, err := exec.LookPath("nmake.exe")
+	if err != nil {
+		return err
+	}
+	if err := execCmd(nmakePath, target); err != nil {
+		return fmt.Errorf("NMake failed: %v", err)
+	}
+
+	// Copy over resources
+	if err := copyResources(qmakePath, target); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func execCmd(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func copyResources(qmakePath string, target string) error {
+	cefDir := os.Getenv("CEF_DIR")
+	if cefDir == "" {
+		return fmt.Errorf("Unable to find CEF_DIR env var")
+	}
+	// Copy over some Qt DLLs
+	err := copyEachToDirIfNotPresent(filepath.Dir(qmakePath), target,
+		"Qt5Core.dll",
+		"Qt5Gui.dll",
+		"Qt5Widgets.dll",
+	)
+	if err != nil {
+		return err
+	}
+
+	// Copy over CEF libs
+	err = copyEachToDirIfNotPresent(filepath.Join(cefDir, strings.Title(target)), target,
+		"libcef.dll",
+		"chrome_elf.dll",
+		"natives_blob.bin",
+		"snapshot_blob.bin",
+		"d3dcompiler_43.dll",
+		"d3dcompiler_47.dll",
+		"libEGL.dll",
+		"libGLESv2.dll",
+	)
+	if err != nil {
+		return err
+	}
+
+	// Copy over CEF resources
+	cefResDir := filepath.Join(cefDir, "Resources")
+	err = copyEachToDirIfNotPresent(cefResDir, target,
+		"icudtl.dat",
+		"cef.pak",
+		"cef_100_percent.pak",
+		"cef_200_percent.pak",
+		"cef_extensions.pak",
+		"devtools_resources.pak",
+	)
+	if err != nil {
+		return err
+	}
+
+	// And CEF locales
+	targetLocaleDir := filepath.Join(target, "locales")
+	if err = os.MkdirAll(targetLocaleDir, os.ModeDir); err != nil {
+		return err
+	}
+	err = copyEachToDirIfNotPresent(filepath.Join(cefResDir, "locales"), targetLocaleDir, "en-US.pak")
+	return err
+}
+
+func copyEachToDirIfNotPresent(srcDir string, destDir string, srcFilenames ...string) error {
+	for _, srcFilename := range srcFilenames {
+		if err := copyToDirIfNotPresent(filepath.Join(srcDir, srcFilename), destDir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func copyToDirIfNotPresent(src string, destDir string) error {
+	return copyIfNotPresent(src, filepath.Join(destDir, filepath.Base(src)))
+}
+
+func copyIfNotPresent(src string, dest string) error {
+	if _, err := os.Stat(dest); os.IsExist(err) {
+		debugLogf("Skipping copying '%v' to '%v' because it already exists")
+		return nil
+	}
+	debugLogf("Copying %v to %v\n", src, dest)
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	cerr := out.Close()
+	if err != nil {
+		return err
+	}
+	return cerr
+}
+
+func debugLogf(format string, v ...interface{}) {
+	if debugEnabled {
+		log.Printf(format, v...)
+	}
+}
---
+++ qt_cef_poc.pro
@@ -0,0 +1,10 @@
+
+QT += core gui widgets
+TARGET = qt_cef_poc
+TEMPLATE = app
+DEFINES += QT_DEPRECATED_WARNINGS
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
