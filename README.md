08ae79e76da67acb51e4f529fbaf77408c308e84
Chad Retz <chad.retz@gmail.com>
2017-06-28 14:21:52 -0500

    Cleaned up build instructions and added packaging

--- README.md
+++ README.md
@@ -1,35 +1,58 @@
 
-## Building on Windows
+# qt_cef_poc
 
-### Prereqs
+Proof of concept of a very simplistic cross-platform Qt browser using Chromium Embedded Framework.
 
-* Qt 5.8+
-* [MSVC 2015 Build Tools](http://landinghub.visualstudio.com/visual-cpp-build-tools)
+## Building
+
+### Windows Prerequisites
+
+Before running the build script on Windows, you must have the prerequisites:
+
+* Latest Qt (5.x) installed w/ `qmake.exe` on the `PATH`
+* Latest CMake installed w/ `cmake.exe` on the `PATH`
+* Latest Python (2.x) installed w/ `python.exe` on the `PATH`
+* Latest Go installed w/ `go.exe` on the `PATH`
+* [MSVC 2015 Build Tools](http://landinghub.visualstudio.com/visual-cpp-build-tools) installed w/ the following
+  executed to put 64-bit VC compiler on the `PATH`:
+  `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64`
 * Latest [Windows 64-bit standard dist of CEF](http://opensource.spotify.com/cefbuilds/index.html#windows64_builds)
-* Latest cmake
-* Python 2.x
-* Go 1.8+
+  extracted w/ `CEF_DIR` environment variable set to the base CEF extracted dir
+* This repo cloned w/ the shell at the repo root
 
-### Building CEF Wrapper
+### Linux Prerequisites
 
-This builds the DLL C++ wrapper that is statically linked to the final app. TODO: automate this too
+(TODO)
 
-* Extract CEF standard dist, cd to dir
-* Make sure 64-bit msvc tools are on path by running `vcvarsall.bat amd64` (e.g.
-  `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64`)
-* cmake -G "Visual Studio 14 Win64" .
-* `cd libcef_dll_wrapper`
-* In libcef_dll_wrapper.vcxproj, under debug's `RuntimeLibrary`, change `MultiThreaded` to `MultiThreadedDebugDLL` and
-  under release's `RuntimeLibrary`, change `MultiThreaded` to `MultiThreadedDLL`
-* For the debug build, `msbuild libcef_dll_wrapper.vcxproj /p:Configuration=Debug`
-* For the release build, `msbuild libcef_dll_wrapper.vcxproj /p:Configuration=Release`
+### MacOS Prerequisites
 
-### Building App
+(TODO)
 
-* Cd to cloned dir
-* Make sure `qmake.exe` from Qt and `go` from Go are on the `PATH`
-* Make sure 64-bit msvc tools are on path by running `vcvarsall.bat amd64` (e.g.
-  `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64`)
-* Make sure env var `CEF_DIR` is set to the dir where CEF was extracted to
-* run `go run build.go build release` (or `run release` to run the release, or `clean` to clean, or replace `release`
-  with `debug` for debug version)
+### Running Build
+
+The application uses the CEF C++ wrapper, which can be built via:
+
+    go run build.go build-cef
+
+Now the application can easily be run via:
+
+    go run build.go run
+
+That will run the `debug` version. For the `release` version, run:
+
+    go run build.go run release
+
+Internally, that just builds and runs the exe. To just build, call `build` instead. To package a deployment artifact
+from a previously run build, `package` is used. For example, to package a `release` deployment artifact from a
+previously run `release` build, run:
+
+    go run build.go package release
+
+## TODO
+
+* Move everything but the exe to a sub folder for cleaner deployment
+  * For CEF, see [this](http://www.magpcss.org/ceforum/viewtopic.php?f=6&t=10296) post
+  * May need to manually `LoadLibrary` and/or use
+    [`SetDllDirectory`](https://msdn.microsoft.com/en-us/library/windows/desktop/ms686203(v=vs.85).aspx)
+  * Consider instead cross platform shortcuts at the top level instead
+  * Chrome does this, see their deployment dir and then look at their code on how they do it
--- build.go
+++ build.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -27,7 +30,7 @@ func start() error {
 		return fmt.Errorf("Unsupported OS '%v' or arch '%v'", runtime.GOOS, runtime.GOARCH)
 	}
 	if len(os.Args) < 2 {
-		return fmt.Errorf("No command provided. Use either 'clean', 'build', or 'run'")
+		return fmt.Errorf("No command provided")
 	}
 
 	switch os.Args[1] {
@@ -49,6 +52,10 @@ func start() error {
 		return err
 	case "build":
 		return build()
+	case "package":
+		return pkg()
+	case "build-cef":
+		return buildCef()
 	default:
 		return fmt.Errorf("Unrecognized command '%v'", os.Args[1])
 	}
@@ -115,6 +122,91 @@ func build() error {
 	return nil
 }
 
+func pkg() error {
+	target, err := target()
+	if err != nil {
+		return err
+	}
+	// Just move over the files that matter to a new deploy dir and zip em up
+	deployDir := filepath.Join(target, "package", "qt_cef_poc")
+	if err = os.MkdirAll(deployDir, os.ModeDir); err != nil {
+		return err
+	}
+
+	// Get all base-dir items to copy, excluding only some
+	filesToCopy := []string{}
+	dirFiles, err := ioutil.ReadDir(target)
+	if err != nil {
+		return err
+	}
+	for _, file := range dirFiles {
+		if !file.IsDir() {
+			switch filepath.Ext(file.Name()) {
+			case ".cpp", ".h", ".obj", ".res", ".manifest", ".log":
+				// No-op
+			default:
+				filesToCopy = append(filesToCopy, file.Name())
+			}
+		}
+	}
+	if err = copyEachToDirIfNotPresent(target, deployDir, filesToCopy...); err != nil {
+		return err
+	}
+
+	// And the locales dir
+	if err = os.MkdirAll(filepath.Join(deployDir, "locales"), os.ModeDir); err != nil {
+		return err
+	}
+	err = copyEachToDirIfNotPresent(filepath.Join(target, "locales"), filepath.Join(deployDir, "locales"), "en-US.pak")
+	if err != nil {
+		return err
+	}
+
+	// Now create a zip file with
+	err = createSingleDirZipFile(deployDir, filepath.Join(target, "package", "qt_cef_poc.zip"))
+	if err != nil {
+		return err
+	}
+
+	return os.RemoveAll(deployDir)
+}
+
+func buildCef() error {
+	cefDir := os.Getenv("CEF_DIR")
+	if cefDir == "" {
+		return fmt.Errorf("Unable to find CEF_DIR env var")
+	}
+	// Build the make files
+	if err := execCmdInDir(cefDir, "cmake", "-G", "Visual Studio 14 Win64", "."); err != nil {
+		return fmt.Errorf("CMake failed: %v", err)
+	}
+
+	// Replace a couple of strings
+	dllWrapperDir := filepath.Join(cefDir, "libcef_dll_wrapper")
+	vcProjFile := filepath.Join(dllWrapperDir, "libcef_dll_wrapper.vcxproj")
+	projXml, err := ioutil.ReadFile(vcProjFile)
+	if err != nil {
+		return fmt.Errorf("Unable to read VC proj file: %v", err)
+	}
+	// First one is debug, second is release
+	projXml = bytes.Replace(projXml, []byte("<RuntimeLibrary>MultiThreaded</RuntimeLibrary>"),
+		[]byte("<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>"), 1)
+	projXml = bytes.Replace(projXml, []byte("<RuntimeLibrary>MultiThreaded</RuntimeLibrary>"),
+		[]byte("<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>"), 1)
+	if err = ioutil.WriteFile(vcProjFile, projXml, os.ModePerm); err != nil {
+		return fmt.Errorf("Unable to write VC proj file: %v", err)
+	}
+
+	// Build debug and then build release
+	if err = execCmdInDir(dllWrapperDir, "msbuild", "libcef_dll_wrapper.vcxproj", "/p:Configuration=Debug"); err != nil {
+		return fmt.Errorf("Unable to build debug wrapper: %v", err)
+	}
+	if err = execCmdInDir(dllWrapperDir, "msbuild", "libcef_dll_wrapper.vcxproj", "/p:Configuration=Release"); err != nil {
+		return fmt.Errorf("Unable to build release wrapper: %v", err)
+	}
+	return nil
+}
+
 func target() (string, error) {
 	target := "debug"
 	if len(os.Args) >= 3 {
@@ -127,7 +219,12 @@ func target() (string, error) {
 }
 
 func execCmd(name string, args ...string) error {
+	return execCmdInDir("", name, args...)
+}
+
+func execCmdInDir(dir string, name string, args ...string) error {
 	cmd := exec.Command(name, args...)
+	cmd.Dir = dir
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
@@ -139,11 +236,11 @@ func copyResources(qmakePath string, target string) error {
 		return fmt.Errorf("Unable to find CEF_DIR env var")
 	}
 	// Copy over some Qt DLLs
-	qtDlls := []string {
+	qtDlls := []string{
 		"Qt5Core.dll",
 		"Qt5Gui.dll",
 		"Qt5Widgets.dll",
-	};
+	}
 	// Debug libs are d.dll
 	if target == "debug" {
 		for i := range qtDlls {
@@ -158,7 +255,7 @@ func copyResources(qmakePath string, target string) error {
 	// Need special ucrtbased.dll for debug builds
 	if target == "debug" {
 		err = copyEachToDirIfNotPresent("C:\\Program Files (x86)\\Windows Kits\\10\\bin\\x64\\ucrt",
-			target, "ucrtbased.dll");
+			target, "ucrtbased.dll")
 		if err != nil {
 			return err
 		}
@@ -243,4 +340,49 @@ func debugLogf(format string, v ...interface{}) {
 	if debugEnabled {
 		log.Printf(format, v...)
 	}
+}
+
+func createSingleDirZipFile(dir string, zipFilename string) error {
+	zipFile, err := os.Create(zipFilename)
+	if err != nil {
+		return err
+	}
+
+	w := zip.NewWriter(zipFile)
+	closed := false
+	defer func() {
+		if !closed {
+			w.Close()
+		}
+	}()
+
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		rel, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+		zipPath := filepath.ToSlash(filepath.Join(filepath.Base(dir), rel))
+		srcPath := filepath.Join(dir, rel)
+
+		dest, err := w.Create(zipPath)
+		if err != nil {
+			return err
+		}
+		src, err := os.Open(srcPath)
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+		_, err = io.Copy(dest, src)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+
+	closed = true
+	return w.Close()
 }
